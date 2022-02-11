abced4bfd6c3ede1aa289d44ede6f6158844eee6
Gerome Pagatpatan <gerome.pagatpatan@mynt.xyz>
2022-02-11 13:49:24 +0800

    Initial Commit

---
+++ RetirementCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RetirementCalculator
+//
+//  Created by Gerome Pagatpatan on 2/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
